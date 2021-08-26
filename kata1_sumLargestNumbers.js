6b3ad5898a922498338315f65296a2c5df702cc5
habib.codes <habib.codes@gmail.com>
2021-08-26 13:50:21 -0400

    used bubble sort to arrange array in ascending order. next will add sum function of two last indices

--- kata1_sumLargestNumbers.js
+++ kata1_sumLargestNumbers.js
@@ -11,14 +11,25 @@ const arr = [1, 7, 2, 13]; // 7+13=20
 
 const sumLargestNumbers = (data) => {
   for (let i = 0; i < data.length; i++) {
-    //
-    for (let j = 0; j < data.length - 1; j++) {
-      //
+    // inner iteration
+    for (let j = 0; j < data.length; j++) {
+      if (data[j] > data[j + 1]) {
+        //initialise a container
+        let temp = data[j];
+        // switch out element at [j] into temp container
+        data[j] = data[j + 1];
+        data[j + 1] = temp;
+      }
     }
   }
-  //use bubble sort!
+  // return array
+  return data;
 };
 
-// console.log(sumLargestNumbers([1, 10]));
-// console.log(sumLargestNumbers([1, 2, 3]));
-// console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+console.log(sumLargestNumbers([1, 3, 12, 4]));
+
+console.log(sumLargestNumbers(arr));
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
