d0cc003854247bb27c7f566b41c476b0c173985c
habib.codes <habib.codes@gmail.com>
2021-08-26 15:21:13 -0400

    clear git stage for commit

--- kata2_conditionalSums.js
+++ kata2_conditionalSums.js
@@ -6,7 +6,17 @@
         - odd --> sum all odd nums
         - even --> sum all even nums
         - edge case --> if no values match condition return 0
-    - you cannot use JS array methods --> use sort algos
+    - you cannot use JS array methods
+
+    tangent reading:
+    - sorting algos: 
+        - bubble --> quadratic = very slow
+        - selection --> quadratic 
+        - insertion --> 
+        - quick --> best for avg cases**
+        - merge -->
+
+        https://www.khanacademy.org/computing/computer-science/algorithms/quick-sort/a/overview-of-quicksort
 */
 
 const conditionalSum = function (values, condition) {
