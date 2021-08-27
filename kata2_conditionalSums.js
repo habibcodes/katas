ee19f4d25143d83d1a1e509939214fc879a27553
habib.codes <habib.codes@gmail.com>
2021-08-27 14:08:31 -0400

    used simple for loop after failing with for...of; issue was placement of return inside the loop instead of outside of it

--- kata2_conditionalSums.js
+++ kata2_conditionalSums.js
@@ -1,12 +1,12 @@
 // Kata 2 - Conditional Sums //
 
 /*
-    - create conditionalSum(values, condition); that adds only two numbers in an array which matches the given condition
+    - create conditionalSum(values, condition); that adds only numbers in an array which matches the given condition
     - there are two conditions:
         - odd --> sum all odd nums
         - even --> sum all even nums
         - edge case --> if no values match condition return 0
-    - you cannot use JS array methods
+    - you cannot use JS array method .filter();
 
     tangent reading:
     - sorting algos: 
@@ -20,10 +20,20 @@
 */
 
 const conditionalSum = function (values, condition) {
-  // Your code here
+  // initialise container outside loop
+  let sum = 0;
+  for (let i = 0; i < values.length; i++) {
+    if (condition === 'even' && values[i] % 2 === 0) {
+      sum += values[i];
+    } else if (condition === 'odd' && values[i] % 2 !== 0) {
+      sum += values[i];
+    }
+  }
+  // return modifications **outside** loop, at end of func!
+  return sum;
 };
 
-console.log(conditionalSum([1, 2, 3, 4, 5], 'even'));
+console.log(conditionalSum([1, 2, 3, 4, 5], 'even')); //6 //9
 console.log(conditionalSum([1, 2, 3, 4, 5], 'odd'));
 console.log(conditionalSum([13, 88, 12, 44, 99], 'even'));
 console.log(conditionalSum([], 'odd'));
