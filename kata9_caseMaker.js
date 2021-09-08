734514c20111a74bf5a03b492c31e70fd881810c
habib.codes <habib.codes@gmail.com>
2021-09-07 22:32:31 -0400

    created the scratch sheet for kata 9

---
+++ kata9_caseMaker.js
@@ -0,0 +1,17 @@
+// Kata 9 - Case Maker //
+
+/*
+    - create a function called camelCase(input) that converts an input string into camelCase text
+    - the input will be a string separated by whitespace
+
+    - similar to 'percentEncoded' --> use trim()/split()/join()
+*/
+const camelCase = function (input) {
+  // use method that removes white space and capitalises first letter after each whitespace
+};
+
+/*
+  console.log(camelCase("this is a string"));
+  console.log(camelCase("loopy lighthouse"));
+  console.log(camelCase("supercalifragalisticexpialidocious"));
+  */
