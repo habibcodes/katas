211b6d6e5f041a7a2b733446877ebe7a28a6f044
habib.codes <habib.codes@gmail.com>
2021-09-09 09:09:13 -0400

    created scratch sheet for kata 10

---
+++ kata10_multiplicationTable.js
@@ -0,0 +1,14 @@
+// Kata 10 - Multiplication Table //
+
+/*
+    - create the func multiplicationTable(maxValue); which takes a single parameter and generates a multiplication table for values from 1 to the provided number
+
+*/
+
+const multiplicationTable = function (maxValue) {
+  // Your code here
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
